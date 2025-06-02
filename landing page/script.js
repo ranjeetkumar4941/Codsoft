f08ac76ea1104b42e24d93a547da623c59d25736
ranjeetkumar4941 <ranjeetkumar49424@gmail.com>
2025-06-02 16:17:27 +0530

    landing page added

---
+++ landing page/script.js
@@ -0,0 +1,9 @@
+// function showAlert() {
+//   alert("Welcome to the Landing Page!");
+// }
+
+
+// Example: Scroll alert
+window.addEventListener('scroll', () => {
+  console.log("Page is being scrolled");
+});
